5975a568cac7209892a4661b526cb6b9bbcb3587
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 22:22:29 +0530

    feat: app.js buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h2>Qr Generator and Scanner</h2>
+      <div>
+        <button>Qr Generator</button>
+        <button>Qr Scanner</button>
+      </div>
     </div>
   );
 }
