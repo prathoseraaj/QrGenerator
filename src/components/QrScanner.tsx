37b615eaed1adc49295285365b9b1d134a9b1f41
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 22:14:43 +0530

    feat: useState

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { QrReader } from "react-qr-reader";
 
 const QrScanner: React.FC = () => {
+
+  const [scanResult, setScanResult] = useState<string>("");
+
   return (
     <div>
       <h2>QR Scanner</h2>
