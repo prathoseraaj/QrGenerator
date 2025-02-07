b9a8088ff9a6229a7a9c0f10b0862fef46ef4c9d
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 23:17:39 +0530

    fix: scanner

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -4,21 +4,40 @@ import { QrReader } from "react-qr-reader";
 const QrScanner: React.FC = () => {
 
   const [scanResult, setScanResult] = useState<string>("");
+  const [cameraactive, setCameraactive] = useState<Boolean>(true);
 
   return (
     <div>
       <h2>QR Scanner</h2>
-      <QrReader
-      onResult={(result,error) =>{
-        if (result){ setScanResult(result.getText())}
-        else{
-          console.error(error);
-        }
-        
-      }}
-      constraints={{facingMode: 'environment'}}
-      />
-      {scanResult && <p>Scanned: {scanResult}</p>}
+      { cameraactive && (
+        <div>
+          <QrReader
+            onResult={(result, error) => {
+              if (result) { setScanResult(result.getText()) }
+              else {
+                console.error(error);
+              }
+
+            }}
+            constraints={{ facingMode: 'environment' }}
+            scanDelay={300}
+          />
+        </div>
+      )}
+      {scanResult && (
+        <div>
+          <h3>Scanned Result:</h3>
+          <p>{scanResult}</p>
+          <button
+            onClick={() => {
+              setScanResult("");
+              setCameraactive(true); 
+            }}
+          >
+            Scan Again
+          </button>
+        </div>
+      )}
     </div>
   )
 }
