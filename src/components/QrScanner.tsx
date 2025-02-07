f6d4c735f79f48be7e63cc422a4ec28a6939dc34
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 22:12:36 +0530

    fix: name

---
+++ src/components/QrScanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { QrReader } from "react-qr-reader";
+
+const QrScanner: React.FC = () => {
+  return (
+    <div>
+      <h2>QR Scanner</h2>
+    
+    </div>
+  )
+}
+
+export default QrScanner
