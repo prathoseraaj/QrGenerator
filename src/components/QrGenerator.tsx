bf5ba009e783798c15383fe80154c3d70bee98cd
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 22:10:26 +0530

    feat: qr generator

--- src/components/QrGenerator.tsx
+++ src/components/QrGenerator.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {QRCodeCanvas}  from "qrcode.react";
 
-const QrGenerator = () => {
+const QrGenerator: React.FC = () => {
+  
+  const [text,setText] = useState(""); 
+
   return (
-    <div>QrGenerator</div>
+    <div>
+        <h2>
+          Qr code Generator
+        </h2>
+        <input  type="text"
+                placeholder='enter text (or) URL'
+                value={text}
+                onChange={(e)=>setText(e.target.value)}
+        />
+        {text && <QRCodeCanvas value={text} size={200} />}
+    </div>
   )
 }
 
