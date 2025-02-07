57b9b7cdfdd2735c51b1ff216ddb7e5ee386b7b4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 23:07:05 +0530

    feat: css

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <div className='container'>
       <h2>Qr Generator and Scanner</h2>
-      <div>
-        <button onClick={() =>setTab('generator') }>Qr Generator</button>
-        <button onClick={() =>setTab('scanner')}>Qr Scanner</button>
+      <div >
+        <button onClick={() =>setTab('generator') } className='buttons'>Qr Generator</button>
+        <button onClick={() =>setTab('scanner')} className='buttons'>Qr Scanner</button>
       </div>
       {tab === 'generator'? <QrGenerator/> : <QrScanner/>}
       </div>
