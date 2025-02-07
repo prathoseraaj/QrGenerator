317e9ec85c314e13a72c2ffe202a8294b8c19ce0
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-07 22:23:59 +0530

    feat: button functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tab,setTab] = useState('generator');
+
   return (
     <div className="App">
       <h2>Qr Generator and Scanner</h2>
       <div>
-        <button>Qr Generator</button>
-        <button>Qr Scanner</button>
+        <button onClick={() =>setTab('generator') }>Qr Generator</button>
+        <button onClick={() =>setTab('scanner')}>Qr Scanner</button>
       </div>
     </div>
   );
