04c44e1f4fb0dac8ed269ae575f9fff2f9ccbbf7
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-08 10:56:53 +0530

    feat : css button

--- src/components/QrGenerator.tsx
+++ src/components/QrGenerator.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 import './QrGenerator.css'
-import {QRCodeCanvas}  from "qrcode.react";
+import { QRCodeCanvas } from "qrcode.react";
+import { BiSearchAlt } from "react-icons/bi";
 
 const QrGenerator: React.FC = () => {
-  
-  const [text,setText] = useState(""); 
+
+  const [text, setText] = useState("");
 
   return (
     <div className='generator'>
-        <h2>
-          Qr code Generator
-        </h2>
-        <input  type="text"
-                placeholder='enter text (or) URL'
-                value={text}
-                onChange={(e)=>setText(e.target.value)}
+      <h2>
+        Qr code Generator
+      </h2>
+      <div className='input-container'>
+        <input type="text"
+          placeholder='enter text (or) URL'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
-        {text && <QRCodeCanvas className='scanner' value={text} size={200} />}
+        <BiSearchAlt className='search-button'/>
+      </div>
+      {text && <QRCodeCanvas className='scanner' value={text} size={200} />}
     </div>
   )
 }
